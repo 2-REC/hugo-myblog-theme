67894bbaf452b256045071f191796d11838471b1
Derek Severin <severinderek@gmail.com>
2026-06-06 12:03:37 +0700

    photoswipe load fix + scratch to store

--- assets/js/load-photoswipe.js
+++ assets/js/load-photoswipe.js
@@ -3,7 +3,7 @@ Put this file in /static/js/load-photoswipe.js
 Documentation and licence at https://github.com/liwenyip/hugo-easy-gallery/
 */
 
-/* Show an alert if this js file has been loaded twice */
+/* show an alert if file has been loaded twice */
 if (window.loadphotoswipejs) {
 	window.alert("You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6")
 } 
@@ -11,43 +11,42 @@ var loadphotoswipejs = 1
 
 /* TODO: Make the share function work */
 $( document ).ready(function() {
-	/*
-	Initialise Photoswipe
-	*/
-	var items = []; // array of slide objects that will be passed to PhotoSwipe()
-	// for every figure element on the page:
+	/* Initialise Photoswipe */
+	var items = [];
+
 	$('figure').each( function() {
-		if ($(this).attr('class') == 'no-photoswipe') return true; // ignore any figures where class="no-photoswipe"
-		// get properties from child a/img/figcaption elements,
+		if ($(this).attr('class') == 'no-photoswipe') {
+			return true;
+		}
+
 		var $figure = $(this),
-			$a 		= $figure.find('a'),
-			$img 	= $figure.find('img'),
-			$src	= $a.attr('href'),
-			$title  = $img.attr('alt'),
-			$msrc	= $img.attr('src');
-		// if data-size on <a> tag is set, read it and create an item
+			$a = $figure.find('a'),
+			$img = $figure.find('img'),
+			$src = $a.attr('href'),
+			$title = $img.attr('alt'),
+			$msrc = $img.attr('src');
+
 		if ($a.data('size')) {
 			var $size 	= $a.data('size').split('x');
 			var item = {
-				src		: $src,
-				w		: $size[0],
-				h 		: $size[1],
-				title 	: $title,
-				msrc	: $msrc
+				src: $src,
+				w: $size[0],
+				h: $size[1],
+				title: $title,
+				msrc: $msrc
 			};
 			console.log("Using pre-defined dimensions for " + $src);
-		// if not, set temp default size then load the image to check actual size
 		} else {
 			var item = {
-				src		: $src,
-				w		: 800, // temp default size
-				h 		: 600, // temp default size
-				title 	: $title,
-				msrc	: $msrc
+				src: $src,
+				w: 800,
+				h: 600,
+				title: $title,
+				msrc: $msrc
 			};
 			console.log("Using default dimensions for " + $src);
-			// load the image to check its dimensions
-			// update the item as soon as w and h are known (check every 30ms)
+
+			/* load the image to check its dimensions & update as soon as w and h are known (check every 30ms) */
 			var img = new Image(); 
 			img.src = $src;
 			var wait = setInterval(function() {
@@ -61,14 +60,18 @@ $( document ).ready(function() {
 				}
 			}, 30);
 	   	}
-		// Save the index of this image then add it to the array
+
 		var index = items.length;
 		items.push(item);
-		// Event handler for click on a figure
+
 		$figure.on('click', function(event) {
-			event.preventDefault(); // prevent the normal behaviour i.e. load the <a> hyperlink
-			// Get the PSWP element and initialise it with the desired options
+			event.preventDefault();
+
 			var $pswp = $('.pswp')[0];
+			if (!$pswp) {
+				console.error("PhotoSwipe root element not found!");
+				return;
+			}
 			var options = {
 				index: index, 
 				bgOpacity: 0.8,
